be0ed2e49435e5b903d8635c43ee16fca7d50757
yota <yas-07@mail.ru>
2019-04-24 23:07:45 +0300

    Задача 4 выполнена.

--- src/Task_1.java
+++ src/Task_1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Task_1 {
     public static void main (String [] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println ("Введите двоичное число:");
+        System.out.println ("Введите1 двоичное число:");
         String bin = sc.nextLine ();
         int binNum = Integer.parseInt (bin, 2);
         System.out.println (binNum);
