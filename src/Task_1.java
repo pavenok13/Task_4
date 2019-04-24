73c0691faf79bf7e53e0296c7949e962625d202c
yota <yas-07@mail.ru>
2019-04-24 23:01:21 +0300

    Задача 4 выполнена.

---
+++ src/Task_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Введите двоичное число:");
+        String bin = sc.nextLine ();
+        int binNum = Integer.parseInt (bin, 2);
+        System.out.println (binNum);
+    }
+}
