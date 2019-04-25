abe1f596e5a3b3defc2019e6f88c379b298ce701
yota <yas-07@mail.ru>
2019-04-25 22:25:23 +0300

    Задача 5

---
+++ src/Task_5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Введите двоичное число:");
+        int Task_4Input  = sc.nextInt();
+        System.out.println ("1" + "*" + Task_4Input + "=" + (1 * Task_4Input) );
+        System.out.println ("2" + "*" + Task_4Input + "=" + (2 * Task_4Input) );
+        System.out.println ("3" + "*" + Task_4Input + "=" + (3 * Task_4Input) );
+        System.out.println ("4" + "*" + Task_4Input + "=" + (4 * Task_4Input) );
+        System.out.println ("5" + "*" + Task_4Input + "=" + (5 * Task_4Input) );
+        System.out.println ("6" + "*" + Task_4Input + "=" + (6 * Task_4Input) );
+        System.out.println ("7" + "*" + Task_4Input + "=" + (7 * Task_4Input) );
+        System.out.println ("8" + "*" + Task_4Input + "=" + (8 * Task_4Input) );
+        System.out.println ("9" + "*" + Task_4Input + "=" + (9 * Task_4Input) );
+        System.out.println ("10" + "*" + Task_4Input + "=" + (10 * Task_4Input) );
+
+    }
+}
