c3952e5c0a22255f094baae237093cd170ec2b7c
yota <yas-07@mail.ru>
2019-04-26 19:19:10 +0300

    Задача 4 выполнена. Вариант 2

--- src/Task_4.java
+++ src/Task_4.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Task_4 {
-    static int a, count = 0, des = 0;
+    static int a, des = 0;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите бинарное число:");
         int Task_4Input  = scan.nextInt();
-        System.out.println("Соответствует десятичному числу:" + Task_4(Task_4Input, count));
+        System.out.println("Соответствует десятичному числу:" + Task_4(Task_4Input, 0));
     }
 
     public static int Task_4(int number, int count) {
         if (number > 0) {
-            a = number % 10;
+            a = number&1;
             number = number / 10;
-            des = des + a * (int) Math.pow(2, count);
+            des = des + (a<<count);
             return Task_4(number, ++count);
         }
         return des;
