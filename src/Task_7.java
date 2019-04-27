c3002fe67cbb48bb96aafb995f1795f509edc864
yota <yas-07@mail.ru>
2019-04-27 20:48:42 +0300

    Задача 7 исправлена.

--- src/Task_7.java
+++ src/Task_7.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 public class Task_7 {
-    static int X=7, Y=13, Z=2;
-    public static void main(String[] args){
-        System.out.println("Введите число:");
+    public static void main(String[] args) {
+        System.out.println("Введите число :");
         Scanner in = new Scanner(System.in);
         int T = in.nextInt();
-        if (T == X){
-            System.out.println("Данное значение имеется в константах:"); }
-        else if (T == Y){
-            System.out.println("Данное значение имеется в константах:"); }
-        else if (T == Z){
-            System.out.println("Данное значение имеется в константах:"); }
-        else   {
-            System.out.println("Такой константы нет:");
+        if (T==13 || T==5 || T==34) {
+            System.out.println("Данное значение имеется в константах:");
+             } else {
+                System.out.println("Такой константы нет:");
+             }
         }
     }
-}
+
