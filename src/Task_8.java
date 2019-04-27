79f00c6e69c7a33774eff1deb2df6ea0d4a487c8
yota <yas-07@mail.ru>
2019-04-27 15:37:24 +0300

    Задача 8.

---
+++ src/Task_8.java
@@ -0,0 +1,9 @@
+
+public class Task_8 {
+    public static void main(String args[]) {
+        for(int x = 1; x < 51; x++) {
+            System.out.print("Значение x: " + x );
+            System.out.print("\n");
+        }
+    }
+}
