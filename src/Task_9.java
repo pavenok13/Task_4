0b3e1434c783708077de79b0080b13df35c9f018
yota <yas-07@mail.ru>
2019-04-27 22:29:40 +0300

    Задача 9.

--- src/Task_9.java
+++ src/Task_9.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
+
 public class Task_9 {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите размер массива:");
+        System.out.println("Введите размер массива: ");
         int size = in.nextInt();
-        int array[]=new int[size];
-        System.out.println("Введите элементы масива:");
-        for (int i=0; i<size; i++);{
-            array[i] = in.nextInt(); }
-        System.out.println("Введите элементы масива:");
-        for(int i=0; i<size; i++){
-            System.out.println(" " + array[i]*2);
+        int array[] = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = in.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + array[i] * 2);
         }
         System.out.println();
     }
