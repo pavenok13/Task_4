12bb51a262a0e158f2998546eb20d8bf103392eb
yota <yas-07@mail.ru>
2019-04-29 19:52:28 +0300

    Задача 14.

---
+++ src/Task_14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку;");
+        String s = sc.nextLine();
+        int x = Integer.parseInt(s);
+        double y = x;
+        System.out.println("Результат");
+        System.out.println( s);
+        System.out.println( x);
+        System.out.println( y);
+
+
+    }
+}
